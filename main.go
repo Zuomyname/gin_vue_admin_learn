d364310d9c27a0b96f0d36a1c5b675f2cf8ae1e5
willzuo <willzuo@tencent.com>
2022-03-22 23:35:12 +0800

    day 2

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"gin_vue_admin_learn/core"
 	"gin_vue_admin_learn/global"
+	"gin_vue_admin_learn/initialize"
 )
 
 func main() {
 	global.GVA_VP = core.Viper()
 	global.GVA_LOG = core.Zap()
-
+	global.GVA_DB = initialize.Gorm()
+	if global.GVA_DB != nil {
+		initialize.RegisterTables(global.GVA_DB) // 初始化表
+		// 程序结束前关闭数据库链接
+		db, _ := global.GVA_DB.DB()
+		defer db.Close()
+	}
+	core.RunWindowsServer()
 }
